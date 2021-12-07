fcc1e95144ee0e4cb708022245d997a3316a0e0c
anvytran-dev <anvytran857@gmail.com>
2021-12-07 13:24:59 -0500

    Updated PUT

--- index.js
+++ index.js
@@ -64,17 +64,26 @@ server.get("/destinations", cors(), (req, res) => {
     res.send(destinations)
 });
 
-server.put("/destinations/:id", (req, res) => {
+server.put("/destinations/", async (req, res) => {
 
-    let destId = req.params.id
-    console.log(destId)
+    const { id, name, location, description } = req.body
 
-    const { id, name, location, photo, description } = req.body
+    if(id === undefined) {
+        return res.status(400).json({ message: "id is required" })
+    }
+
+    if(name !== undefined && name.length === 0 ) {
+        return res.status(400).json({ message: "name can't be empty" })
+    }
+
+    if(location !== undefined && location.length === 0 ) {
+        return res.status(400).json({ message: "location can't be empty" })
+    }
 
     for (const dest of destinations) {
         if (dest.id === destId) {
 
-            if (name !== undefined && name.length !== 0) {
+            if (name !== undefined) {
                 dest.name = name
             }
 
@@ -82,8 +91,11 @@ server.put("/destinations/:id", (req, res) => {
                 dest.location = location;
             }
 
-            if (photo !== undefined) {
-                dest.photo = photo;
+            if(name !== undefined || location !== undefined) {
+                dest.photo = await getUnsplashPhoto({
+                    name: dest.name,
+                    location: dest.location
+                })
             }
 
             if (description !== undefined) {
@@ -91,10 +103,7 @@ server.put("/destinations/:id", (req, res) => {
             }
         }
     }
-
-    console.log(destinations)
-    res.send(destinations)
-    return res.json()
+    return res.json(dest)
 
 })
 
@@ -106,5 +115,22 @@ server.delete("/destinations", (req, res) => {
 
     destinations = newDestinations
     res.send(destinations)
-    res.redirect("/destinations")
+    res.redirect( 303, "/destinations")
 })
+
+function getUnsplashPhoto(name, location) {
+    const API_KEY = "2vAoc_zs73pmQGlzynGD8AsbJmMCu-V3TjCKc5Kqu3I"
+    const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=QsPtItqyx9-VpUsYx2K7DlLKcxJ-Kg6EthZ1LimBMy8&query=${name}`
+    console.log(UNSPLASH_URL)
+
+    try {
+        const res = await fetch(UNSPLASH_URL)
+        const data = await res.json()
+
+        return data.urls.small
+
+    } catch (error) {
+        //placeholder photo
+        console.log("error")
+    }
+}
